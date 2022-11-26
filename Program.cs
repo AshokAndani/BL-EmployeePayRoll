b820127d183690d15dc2b3671739eb82955db1ce
ashokk <ashokk@nextgen.com>
2022-11-26 17:58:33 +0530

    added UC2 code changes

--- Program.cs
+++ Program.cs
@@ -9,10 +9,19 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             Random random = new Random();
-            int number=random.Next(0, 2);
+            int number=random.Next(0, 3);
+            int WagePerHour = 20;
+            int FullDayHour = 8;
+            int partTimeHour = 4;
             if(number == 0)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present and Worked Full-Time");
+                Console.WriteLine("Wage is: "+WagePerHour*FullDayHour);
+            }
+            else if(number==1)
+            {
+                Console.WriteLine("Employee is Present and Worked Part-Time");
+                Console.WriteLine("Wage is: "+WagePerHour*partTimeHour);
             }
             else
             {
