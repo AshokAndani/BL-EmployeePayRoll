a1d635cbdd2e1a0244eace946f86ad5f75b47a3f
ashokk <ashokk@nextgen.com>
2022-11-26 19:56:23 +0530

    code changes for UC7

---
+++ EmployeeWageCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    public class EmployeeWageCalculator
+    {
+        static int WagePerHour = 20;
+        static int FullDayHour = 8;
+        static int partTimeHour = 4;
+        static int workingDaysPerMonth = 20;
+        static int noOfDaysPresent = 20;
+       public int totalHours = 0;
+       public int totalDaysPresent = 0;
+        public int GetEmployeeWage()
+        {
+            int totalWage = 0;
+            Random random = new Random();
+            for (int i = 0; i < workingDaysPerMonth && totalHours <= 100; i++)
+            {
+                int number = random.Next(0, 3);
+                switch (number)
+                {
+                    // 0 for full time
+                    case 0:
+                        totalDaysPresent++;
+                        totalHours += FullDayHour;
+                        totalWage = totalWage + (FullDayHour * WagePerHour);
+                        break;
+                    // 1 for partime
+                    case 1:
+                        totalDaysPresent++;
+                        totalHours += partTimeHour;
+                        totalWage += (partTimeHour * WagePerHour);
+                        break;
+                    //2 for absent
+                    case 2:
+                        totalWage += 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return totalWage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,41 +8,15 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-            Random random = new Random();
-            int WagePerHour = 20;
-            int FullDayHour = 8;
-            int partTimeHour = 4;
-            int workingDaysPerMonth = 20;
-            int totalWage = 0;
-            int noOfDaysPresent = 20;
-            int totalHours = 0;
-            for(int i = 0; i < workingDaysPerMonth && totalHours<=100; i++)
-            {
-                int number = random.Next(0, 3);
-                switch (number)
-                {
-                    // 0 for full time
-                    case 0:
-                        totalHours += FullDayHour;
-                        totalWage = totalWage + (FullDayHour * WagePerHour);
-                        break;
-                    // 1 for partime
-                    case 1:
-                        totalHours += partTimeHour;
-                        totalWage+=(partTimeHour*WagePerHour);
-                        break;
-                    //2 for absent
-                    case 2:
-                        noOfDaysPresent--;
-                        totalWage += 0;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            Console.WriteLine("Number of Hours Employee worked: "+totalHours);
-            Console.WriteLine("Number of Days Employee is Present: "+noOfDaysPresent+" Days");
-            Console.WriteLine("total wages for the month is: " + totalWage);
+
+            EmployeeWageCalculator wageCalculator = new EmployeeWageCalculator();
+            int totalWage=wageCalculator.GetEmployeeWage();
+            int totalPresentDays = wageCalculator.totalDaysPresent;
+            int totalHours = wageCalculator.totalHours;
+           
+            Console.WriteLine("Number of Hours Employee worked: "+totalHours+" hrs");
+            Console.WriteLine("Number of Days Employee is Present: "+totalPresentDays+" Days");
+            Console.WriteLine("total wages for the month is: " + totalWage+"/-");
             Console.ReadKey();
         }
     }
