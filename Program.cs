563b324950f2e66a888d64854bed5a72ae7b695b
ashokk <ashokk@nextgen.com>
2022-11-26 18:20:31 +0530

    UC5 code changes

--- Program.cs
+++ Program.cs
@@ -9,26 +9,36 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             Random random = new Random();
-            int number = random.Next(0, 3);
             int WagePerHour = 20;
             int FullDayHour = 8;
             int partTimeHour = 4;
-            switch (number)
+            int workingDaysPerMonth = 20;
+            int totalWage = 0;
+            int noOfDaysPresent = 20;
+            for(int i = 0; i < workingDaysPerMonth; i++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Present and Worked Full-Time");
-                    Console.WriteLine("Wage is: " + WagePerHour * FullDayHour);
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Present and Worked Part-Time");
-                    Console.WriteLine("Wage is: " + WagePerHour * partTimeHour);
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Absent");
-                    break;
-                default:
-                    break;
+                int number = random.Next(0, 3);
+                switch (number)
+                {
+                    // 0 for full time
+                    case 0:
+                        totalWage = totalWage + (FullDayHour * WagePerHour);
+                        break;
+                    // 1 for partime
+                    case 1:
+                        totalWage+=(partTimeHour*WagePerHour);
+                        break;
+                    //2 for absent
+                    case 2:
+                        noOfDaysPresent--;
+                        totalWage += 0;
+                        break;
+                    default:
+                        break;
+                }
             }
+            Console.WriteLine("Number of Days Employee is Present: "+noOfDaysPresent+" Days");
+            Console.WriteLine("total wages for the month is: " + totalWage);
             Console.ReadKey();
         }
     }
